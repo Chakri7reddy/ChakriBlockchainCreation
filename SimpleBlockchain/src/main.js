a6eca533ae758f70aa0c6886884b516b50665190
Chakri7reddy <129921977+Chakri7reddy@users.noreply.github.com>
2025-06-08 23:44:03 +0530

    Update main.js

--- SimpleBlockchain/src/main.js
+++ SimpleBlockchain/src/main.js
@@ -3,7 +3,7 @@ const { Blockchain, Transaction } = require("./blockchain");
 const EC = require("elliptic").ec;
 const ec = new EC("secp256k1");
 
-// Your private key goes here
+// our private key goes here
 const myKey = ec.keyFromPrivate(
   "7c4c45907dec40c91bab3480c39032e90049f1a44f3e18c3e07c23e3273995cf"
 );
